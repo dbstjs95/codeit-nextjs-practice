fa288c3599522cd1d4231f464483ccaf62d4032a
dbstjs95 <choji95@naver.com>
2023-06-23 02:12:47 +0900

    NotFound page 추가

--- components/ButtonLink.js
+++ components/ButtonLink.js
@@ -1,3 +1,12 @@
-export default function ButtonLink() {
-  return <div>Enter</div>;
+import Link from "next/link";
+import buttonStyles from "./Button.module.css";
+import styles from "./ButtonLink.module.css";
+
+export default function ButtonLink({ className = "", ...props }) {
+  return (
+    <Link
+      className={`${buttonStyles.button} ${styles.buttonLink} ${className}`}
+      {...props}
+    />
+  );
 }
