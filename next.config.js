13ebb545bda325fc9df1322de02f2587103685cc
dbstjs95 <choji95@naver.com>
2023-06-23 01:35:00 +0900

    리다이렉트 설정을 위해 next.config.js 수정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
+// next.js 서버를 설정하는 파일로, 해당 파일을 수정하면 next.js 서버를 껐다 켜야함.(??? 껐다 키지않아도 잘 적용되는데...?)
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/products/:id",
+        destination: "/items/:id",
+        // permanent는 웹브라우저에게 주소가 바뀌었음을 저장하게 하는 코드
+        permanent: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
