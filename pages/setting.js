32c9088cdfd30185681f6fd08c94014781bcbca3
dbstjs95 <choji95@naver.com>
2023-06-23 07:00:18 +0900

    ThemaContext 생성

--- pages/setting.js
+++ pages/setting.js
@@ -1,14 +1,17 @@
-import Container from "@/components/Container";
-import Header from "@/components/Header";
+import DropDown from "@/components/DropDown";
+import { useTheme } from "@/lib/ThemeContext";
 import styles from "@/styles/Setting.module.css";
 
 export default function setting() {
+  const { theme, setTheme } = useTheme();
+
   return (
-    <>
+    <div>
       <h1 className={styles.title}>설정</h1>
       <section className={styles.section}>
         <h2 className={styles.sectionTitle}>테마 설정</h2>
+        <DropDown />
       </section>
-    </>
+    </div>
   );
 }
